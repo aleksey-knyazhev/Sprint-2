a288e46bf73d75df689bfa5e62a7fe92b3f9637b
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-08-22 02:56:50 +0300

    ProblemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -2,7 +2,18 @@ class ProblemA {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val lines = readInt()
+
+        for (i in 1..lines) {
+            val array = readIntArray(3)
+            var position: Long = 0
+            for (counter in 0 until array[2]) if (counter % 2 == 0) {
+                position += array[0]
+            } else {
+                position -= array[1]
+            }
+            println(position)
+        }
 
     }
 
