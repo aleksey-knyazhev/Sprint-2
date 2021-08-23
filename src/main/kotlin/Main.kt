779b64935b8b5325e34af75e42345c5d88440a91
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-08-23 23:39:35 +0300

    Done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,3 @@
-//import java.io.ByteArrayOutputStream
-//import java.io.InputStream
-//import java.io.PrintStream
-
-
 fun main() {
-
-//    val workDir = "problemA"
-//
-//    lateinit var outputStream: ByteArrayOutputStream
-//    lateinit var inputStream: InputStream
-//
-//    outputStream = ByteArrayOutputStream()
-//    val ps = PrintStream(outputStream)
-//    System.setOut(ps)
-//    inputStream = this::class.java.getResource("$workDir/input.txt")!!.openStream()
-//    System.setIn(inputStream)
-
     ProblemD().main()
-
 }
