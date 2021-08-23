c0d10de9e460456727d459bebfea1e62dffef1f3
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-08-23 13:00:22 +0300

    ProblemB

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -1,63 +1,34 @@
-import java.io.ByteArrayOutputStream
-import java.io.InputStream
-import java.io.PrintStream
-
 class ProblemB {
 
     fun main() {
-        var outputStream: ByteArrayOutputStream
-        var inputStream: InputStream
-        val workDir = "problemB"
-
-        outputStream = ByteArrayOutputStream()
-        val ps = PrintStream(outputStream)
-        System.setOut(ps)
-        inputStream = this::class.java.getResource("$workDir/input.txt")!!.openStream()
-        System.setIn(inputStream)
-
 
         val lines = readInt()
 
         for (i in 1..lines) {
             val array = readIntArray(2)
             val stringSize = array[0]
-            var string = ""
             val vocabularySize = array[1]
+            val countOfMaxFrequencyLetters = stringSize % vocabularySize
             val vocabulary = getVocabulary(vocabularySize)
-            val countOfMaxFrequency = stringSize % vocabularySize
-            val minAffordableFrequency = minAffordableFrequency(stringSize, vocabularySize, countOfMaxFrequency)
+            var string = ""
 
-            for (item in vocabulary) {
-                if (countOfMaxFrequency == 0) {
-
-
+            for (i in 1..stringSize / vocabularySize) {
+                string += vocabulary
+            }
+            if (countOfMaxFrequencyLetters != 0) {
+                for (i in 0 until countOfMaxFrequencyLetters) {
+                    string += vocabulary[i].toString()
                 }
             }
-
-
-
-
-
-
-            println(string + "minAffordableFrequency: $minAffordableFrequency")
+            println(string)
         }
-
     }
 
-    private fun getVocabulary(vocabularySize: Int): Array<Char> {
-        val firstPosition = 97 // ASCII символа 'a'
-        var vocabulary: Array<Char> = Array(vocabularySize) {
-            i -> (firstPosition + i).toChar()
-        }
+    private fun getVocabulary(vocabularySize: Int): String {
+        val firstPosition = 97 // ASCII-код символа 'a'
+        var vocabulary = ""
+        for (i in firstPosition until firstPosition + vocabularySize)
+            vocabulary += i.toChar().toString()
         return vocabulary
     }
-
-    private fun minAffordableFrequency(stringSize: Int, vocabularySize: Int, countOfMaxFrequency:Int): Int {
-        var frequency = 0
-        frequency = stringSize / vocabularySize
-        if (countOfMaxFrequency != 0) frequency++
-        return frequency
-    }
-
-
 }
