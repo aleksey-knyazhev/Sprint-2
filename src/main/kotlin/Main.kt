a632a772ce31e5e8a70208842e783edf1106e4a2
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-08-25 22:31:36 +0300

    Updated A

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,3 @@
 fun main() {
-    ProblemD().main()
+    ProblemA().main()
 }
--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -6,7 +6,8 @@ class ProblemA {
         for (i in 1..lines) {
             val array = readIntArray(3)
             var position: Long = 0
-            for (counter in 0 until array[2]) if (counter % 2 == 0) {
+            val countOfJumps = array[2]
+            for (counter in 0 until countOfJumps) if (counter % 2 == 0) {
                 position += array[0]
             } else {
                 position -= array[1]
